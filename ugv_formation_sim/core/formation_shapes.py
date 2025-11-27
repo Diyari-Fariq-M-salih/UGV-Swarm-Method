80350765b408500bc23a83fa396b1cc9c7c2e994
Diyari-Fariq-M-salih <diyari.m.salih@gmail.com>
2025-11-27 21:48:19 +0100

    Update formation_shapes.py
    replaced placeholder shapes

--- ugv_formation_sim/core/formation_shapes.py
+++ ugv_formation_sim/core/formation_shapes.py
@@ -9,12 +9,13 @@ def diamond(d=1.5):
     return [
         (0, 0),
         (-d, -d),
-        (d, -d)
-    ]
+        (d, -d),
+        (0, -2*d)
+    ][:3]
 
 def arrow(d=1.5):
     return [
         (0, 0),
-        (-d, -d),
-        (d, -d)
+        (-d*1.5, -d),
+        (d*1.5, -d)
     ]
